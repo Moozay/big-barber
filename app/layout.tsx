f981970c3609afd843cf1b03273ac16f05f850b7
Musa Haruna <musaharuna679@gmail.com>
2024-12-13 02:37:02 +0100

    fix ui  for smaller screens

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
@@ -35,17 +31,17 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center w-full">
             <div className="flex-1 w-full flex flex-col items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex items-center text-3xl font-semibold ">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 px-4 sm:px-8">
+                <div className="w-full max-w-5xl flex justify-between items-center py-3 px-5 text-sm sm:text-base">
+                  <div className="flex items-center text-2xl sm:text-3xl font-semibold">
                     <Link href={"/"}>BIG BARBER SHOP</Link>
                   </div>
                   <HeaderAuth />
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 p-5 w-full">{children}</div>
+              <div className="flex flex-col gap-10 sm:gap-16 px-4 sm:px-8 py-6 w-full">{children}</div>
 
-              <Footer/>
+              <Footer />
             </div>
           </main>
         </ThemeProvider>
--- components/hero.tsx
+++ components/hero.tsx
@@ -5,23 +5,29 @@ import SupabaseLogo from "./supabase-logo";
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 bg-cover bg-center bg-no-repeat h-screen text-white bg-blend-darken bg-black bg-opacity-50 w-full"
+    <div
+      className="flex flex-col gap-8 bg-cover bg-center bg-no-repeat h-screen text-white bg-blend-darken bg-black bg-opacity-50 w-full"
       style={{ backgroundImage: "url('/imgs/hero-banner.jpg')" }}
     >
-      <div className="flex flex-col items-center">
-      <div className="flex p-20 mt-5 text-9xl font-bold text-center">BARBERS & HAIR CUTTING</div>
-      <div className="flex p-20 font-bold leading-relaxed w-2/3 text-center">SINCE 1999, V's BARBERSHOP HAS BEEN ON A MISSION TO REDEFINE THE MEN'S GROOMING INDUSTRY. WITH OVER 60 AUTHENTIC BARBERSHOPS NATIONWIDE, A CURATED SELECTION OF MEN'S GROOMING PRODUCTS AND PREMIUM SERVICE OFFERINGS... V's IS BRINGING BACK HANDSOME. ONE GENTLEMAN AT A TIME.</div>
-      <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              className="flex max-w-60 bg-orange-400 hover:bg-orange-300"
-            >
-              <Link href="/reservation">CREATE RESERVATION</Link>
-            </Button>
+      <div className="flex flex-col items-center text-center px-4 md:px-16">
+        <div className="flex p-8 mt-5 text-4xl md:text-6xl lg:text-9xl font-bold">
+          BARBERS & HAIR CUTTING
+        </div>
+        <div className="flex p-4 md:p-8 font-bold text-base md:text-lg lg:text-xl leading-relaxed w-full md:w-2/3 lg:w-1/2">
+          SINCE 1999, V's BARBERSHOP HAS BEEN ON A MISSION TO REDEFINE THE MEN'S GROOMING INDUSTRY. WITH OVER 60
+          AUTHENTIC BARBERSHOPS NATIONWIDE, A CURATED SELECTION OF MEN'S GROOMING PRODUCTS AND PREMIUM SERVICE
+          OFFERINGS... V's IS BRINGING BACK HANDSOME. ONE GENTLEMAN AT A TIME.
+        </div>
+        <Button
+          asChild
+          size="sm"
+          variant={"default"}
+          className="flex max-w-[200px] bg-orange-400 hover:bg-orange-300 mt-6"
+        >
+          <Link href="/reservation">CREATE RESERVATION</Link>
+        </Button>
       </div>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-     
     </div>
   );
 }
